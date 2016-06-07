ede4c77cb14bc8e8d8fc88588d85139846ca87f5
erramuzpe <asier.erramuzpe@gmail.com>
2016-06-07 10:37:01 +0200

    nearpy working, testing scikit solutions

--- neurovault/apps/statmaps/management/commands/ann.py
+++ neurovault/apps/statmaps/management/commands/ann.py
@@ -8,54 +8,57 @@ import os
 import numpy as np
 
 
+def createFeatures(subjects=None):
+    if os.path.isfile('/code/neurovault/apps/statmaps/tests/features.npy') and subjects == None:
+        return np.load('/code/neurovault/apps/statmaps/tests/features.npy')
+    else:
+        u1 = User.objects.create(username='neurovault3')
+        i = 1
+        features = np.empty([28549, subjects])
+        for file in os.listdir('/code/neurovault/apps/statmaps/tests/bench/unthres/'):
+            randomCollection = Collection(name='random' + file, owner=u1, DOI='10.3389/fninf.2015.00008' + str(i))
+            randomCollection.save()
+            image = save_statmap_form(image_path=os.path.join('/code/neurovault/apps/statmaps/tests/bench/unthres/', file),
+                                      collection=randomCollection, image_name=file, ignore_file_warning=True)
+            if not image.reduced_representation or not os.path.exists(image.reduced_representation.path):
+                image = save_resampled_transformation_single(image.pk)
+            features[:, i] = np.load(image.reduced_representation.file)
+            i += 1
+            if i == subjects:
+                np.save('/code/neurovault/apps/statmaps/tests/features.npy', features)
+                return features
+
+
 class Command(BaseCommand):
     args = '<times_to_run>'
     help = 'bench'
-    clearDB()
-    app_path = '/code/neurovault/apps/statmaps/tests'
-    u1 = User.objects.create(username='neurovault3')
-    i = 1
-    subjects = 4
-    X = np.empty([28549, subjects])
 
-    for file in os.listdir(os.path.join(app_path, 'bench/unthres/')):
-        randomCollection = Collection(name='random' + file, owner=u1, DOI='10.3389/fninf.2015.00008' + str(i))
-        randomCollection.save()
+    def handle(self, *args, **options):
+        clearDB()
+        features = createFeatures().T #TODO: pass args to this function
 
-        image = save_statmap_form(image_path=os.path.join(app_path, 'bench/unthres/', file),
-                                  collection=randomCollection, image_name=file, ignore_file_warning=True)
-        if not image.reduced_representation or not os.path.exists(image.reduced_representation.path):
-            image = save_resampled_transformation_single(image.pk)
+        n_bits = 10
+        hash_counts = 5
+        metric = "euclidean"
+        name = 'NearPy(n_bits=%d, hash_counts=%d)' % (n_bits, hash_counts)
+        # fit
+        import nearpy, nearpy.hashes, nearpy.distances
 
-        X[:, i] = np.load(image.reduced_representation.file)
-        i = i + 1
-        if i == subjects:
-            np.save(os.path.join(app_path, 'bench'), X)
-            break
+        hashes = []
 
+        # doesn't seem like the NearPy code is using the metric??
+        for k in xrange(hash_counts):
+            nearpy_rbp = nearpy.hashes.RandomBinaryProjections('rbp_%d' % k, n_bits)
+            hashes.append(nearpy_rbp)
 
-    n_bits = 10
-    hash_counts = 5
-    metric = "euclidean"
-    name = 'NearPy(n_bits=%d, hash_counts=%d)' % (n_bits, hash_counts)
-    # fit
-    import nearpy, nearpy.hashes, nearpy.distances
+        nearpy_engine = nearpy.Engine(features.shape[1], lshashes=hashes)
 
-    hashes = []
-
-    # doesn't seem like the NearPy code is using the metric??
-    for k in xrange(hash_counts):
-        nearpy_rbp = nearpy.hashes.RandomBinaryProjections('rbp_%d' % k, n_bits)
-        hashes.append(nearpy_rbp)
-
-    nearpy_engine = nearpy.Engine(X.shape[1], lshashes=hashes)
-
-    for i, x in enumerate(X):
-        nearpy_engine.store_vector(x.tolist(), i)
-
-    def query(self, v, n):
-        return [y for x, y, z in self._nearpy_engine.neighbours(v)]
-
+        for i, x in enumerate(features):
+            nearpy_engine.store_vector(x.tolist(), i)
+        #query
+        results = nearpy_engine.neighbours(features[3])
+        print results
+        #results are the N-nearest neighbours! [vector, data_idx, distance]. (for now, distance is NaN)
 
 
 
